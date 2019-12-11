2127624158d8d05c17944f92068370453c8c626d
Joe <burtonjae@hotmail.co.uk>
2019-12-11 21:10:35 +0000

    Make KnotClassLoaderInterface public

--- src/main/java/net/fabricmc/loader/launch/knot/KnotClassLoaderInterface.java
+++ src/main/java/net/fabricmc/loader/launch/knot/KnotClassLoaderInterface.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
-interface KnotClassLoaderInterface {
+public interface KnotClassLoaderInterface {
 	KnotClassDelegate getDelegate();
 	boolean isClassLoaded(String name);
 	void addURL(URL url);
